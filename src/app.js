b3c2b385a11a21c7ad61d864b1624efd3d75ee43
deveshcse <d.mishra56603@gmail.com>
2025-01-18 22:57:42 +0530

    projectsetup: added healthcheck route

--- src/app.js
+++ src/app.js
@@ -13,4 +13,15 @@ app.use(
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express("public"));
+
+
+// import routes
+
+import healthcheckRouter from "./routes/healthcheck.routes.js"
+
+
+// route
+
+app.use("/api/v1/healthcheck", healthcheckRouter)
+
 export { app };
