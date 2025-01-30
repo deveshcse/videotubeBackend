a45c0dd9a05a91d89984f0f6d5f4f661cabbb4e8
deveshcse <d.mishra56603@gmail.com>
2025-01-30 18:31:28 +0530

    feat: add healthcheck route in app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
+
+
 const app = express();
 
 app.use(
@@ -9,10 +12,11 @@ app.use(
   })
 );
 
-// comman middlewares
+// common middlewares
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express("public"));
+app.use(cookieParser());
 
 
 // import routes
